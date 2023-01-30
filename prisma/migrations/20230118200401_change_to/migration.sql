2316dc0a30631dccaa4d591d4cb059d4f925a7ff
lgili <luiz.gili@posgrad.ufsc.br>
2023-01-30 12:08:24 -0300

    feat: add some unit tests and upgrade yarn

--- prisma/migrations/20230118200401_change_to/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "PermissionsOnRoles" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "roleId" TEXT,
-    "permissionId" TEXT,
-    "assignedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "PermissionsOnRoles_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "roles" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT "PermissionsOnRoles_permissionId_fkey" FOREIGN KEY ("permissionId") REFERENCES "permissions" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
